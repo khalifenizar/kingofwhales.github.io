bef1cf645b73058abbae037562bd6c785483c5cf
kingOFWhales <wileyfan1992@gmail.com>
2016-05-09 21:47:04 -0500

    change argument name to data

--- web/vis.js
+++ web/vis.js
@@ -508,7 +508,7 @@ var updateData = function(choice){
 
 
 
-d3.json("kingofwhales.github.io/res/demographics.json",function(error,json){
+d3.json("kingofwhales.github.io/res/demographics.json",function(error,data){
 		if (error) return console.warn(error); 
 //	console.log(data);
 
